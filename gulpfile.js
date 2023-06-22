db2085d5a9bec6ef24737362a4004b43946524d8
Adeleye Adeyemi <adeleye.adeyemi@jetex.com>
2023-06-22 13:07:08 +0400

    Font reversed

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,9 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-const fontLoaderConfig = {
-    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    use: [{
-        loader: 'file-loader',
-        options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/'
-        }
-    }]
-};
-
-build.configureWebpack.mergeConfig({
-    additionalConfiguration: (generatedConfiguration) => {
-
-        generatedConfiguration.module.rules.push(fontLoaderConfig);
-
-        return generatedConfiguration;
-
-    }
-
-});
-
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
-    
+
 
     var result = getTasks.call(build.rig);
 
